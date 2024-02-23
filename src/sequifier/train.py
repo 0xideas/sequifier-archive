24f634d7ade46f00cc62463df7a646ff8d0ebe0f
Leon Luithlen <leontimnaluithlen@gmail.com>
2024-02-23 01:58:03 +0100

    Update dependencies, unconfirmed

--- src/sequifier/train.py
+++ src/sequifier/train.py
@@ -203,7 +203,7 @@ class TransformerModel(nn.Module):
             x,  # model input (or a tuple for multiple inputs)
             export_path,  # where to save the model (can be a file or file-like object)
             export_params=True,  # store the trained parameter weights inside the model file
-            opset_version=10,  # the ONNX version to export the model to
+            opset_version=14,  # the ONNX version to export the model to
             do_constant_folding=True,  # whether to execute constant folding for optimization
             input_names=["input"],  # the model's input names
             output_names=["output"],  # the model's output names
