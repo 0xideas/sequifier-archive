11aab69287299ae50134b96c6e109b731935d5ef
Leon Luithlen <leontimnaluithlen@gmail.com>
2024-03-20 20:10:38 +0100

    Move multiprocessing into Preprocessing class

--- src/sequifier/preprocess.py
+++ src/sequifier/preprocess.py
@@ -16,14 +16,12 @@ class Preprocessor(object):
         self,
         project_path,
         data_path,
-        data,
         group_proportions,
         seq_length,
         seed,
         target_column,
         return_targets,
         max_rows=None,
-        process_id=0,
     ):
         self.project_path = project_path
         self.seed = seed
@@ -31,12 +29,17 @@ class Preprocessor(object):
 
         os.makedirs(os.path.join(project_path, "data"), exist_ok=True)
 
+        data = pd.read_csv(data_path, sep=",", decimal=".", index_col=None)
+
+        if max_rows is not None:
+            data = data.head(int(max_rows))
+
         self.data_name_root = os.path.split(data_path)[1].split(".")[0]
         self.split_paths = [
             os.path.join(
                 self.project_path,
                 "data",
-                f"{self.data_name_root}-split{i}-{process_id}.csv",
+                f"{self.data_name_root}-split{i}.csv",
             )
             for i in range(len(group_proportions))
         ]
@@ -50,7 +53,7 @@ class Preprocessor(object):
         for data_col in data_columns:
             dtype = str(data[data_col].dtype)
             if dtype in ["object", "int64"]:
-                data, sup_id_map = self.replace_ids(data, column=data_col)
+                data, sup_id_map = replace_ids(data, column=data_col)
                 id_maps[data_col] = dict(sup_id_map)
                 n_classes[data_col] = len(np.unique(data[data_col])) + 1
             elif dtype in ["float64"]:
@@ -63,29 +66,30 @@ class Preprocessor(object):
         self.export_metadata(id_maps, n_classes, col_types)
 
         data = data.sort_values(["sequenceId", "itemPosition"])
-        sequence_ids = sorted(list(np.unique(data["sequenceId"])))
-        for i, sequence_id in enumerate(sequence_ids):
-            data_subset = data.loc[data["sequenceId"] == sequence_id, :]
-            sequences = self.extract_sequences(
-                data_subset, seq_length, data_columns, target_column, return_targets
+
+        n_cores = multiprocessing.cpu_count()
+
+        batch_limits = get_batch_limits(data, n_cores)
+        batches = [
+            (
+                data.iloc[start:end, :],
+                self.split_paths,
+                seq_length,
+                data_columns,
+                target_column,
+                return_targets,
+                group_proportions,
             )
+            for start, end in batch_limits
+            if (end - start) > 0
+        ]
+        batches = [(i, *v) for i, v in enumerate(batches)]
 
-            splits = self.extract_data_subsets(sequences, group_proportions)
-            splits = [self.cast_columns_to_string(data) for data in splits]
+        n_cores_used = len(batches)
+        with multiprocessing.Pool(processes=n_cores_used) as pool:
+            pool.starmap(preprocess_batch, batches)
 
-            for split_path, split in zip(self.split_paths, splits):
-                write_mode = "w" if i == 0 else "a"
-                header = i == 0
-                split.to_csv(
-                    split_path,
-                    mode=write_mode,
-                    header=header,
-                    sep=",",
-                    decimal=".",
-                    index=None,
-                )
-
-        print(f"Written data to {self.split_paths}")
+        combine_multiprocessing_outputs(len(group_proportions), n_cores_used)
 
     def export_metadata(self, id_maps, n_classes, col_types):
 
@@ -107,125 +111,147 @@ class Preprocessor(object):
         ) as f:
             f.write(json.dumps(data_driven_config))
 
-    @classmethod
-    def cast_columns_to_string(cls, data):
-        data.columns = [str(col) for col in data.columns]
-        return data
 
-    @classmethod
-    def replace_ids(cls, data, column):
-        ids = sorted(
-            [int(x) if not isinstance(x, str) else x for x in np.unique(data[column])]
-        )
-        id_map = {id_: i + 1 for i, id_ in enumerate(ids)}
-        data[column] = data[column].map(id_map)
-        return (data, id_map)
+def cast_columns_to_string(data):
+    data.columns = [str(col) for col in data.columns]
+    return data
 
-    @classmethod
-    def extract_subsequences(
-        cls, in_seq, seq_length, data_columns, target_column, return_targets
-    ):
 
-        if return_targets:
-            nseq = max(
-                len(in_seq[target_column]) - seq_length - 1,
-                min(1, len(in_seq[target_column])),
-            )
+def replace_ids(data, column):
+    ids = sorted(
+        [int(x) if not isinstance(x, str) else x for x in np.unique(data[column])]
+    )
+    id_map = {id_: i + 1 for i, id_ in enumerate(ids)}
+    data[column] = data[column].map(id_map)
+    return (data, id_map)
 
-            targets = [in_seq[target_column][i + seq_length] for i in range(nseq)]
-        else:
-            nseq = max(
-                len(in_seq[target_column]) - seq_length,
-                min(1, len(in_seq[target_column])),
-            )
-            targets = [np.nan for _ in range(nseq)]
 
-        seqs = {}
-        for data_col in data_columns:
-            seqs[data_col] = [in_seq[data_col][i : i + seq_length] for i in range(nseq)]
+def extract_subsequences(
+    in_seq, seq_length, data_columns, target_column, return_targets
+):
 
-        if len(seqs[target_column]) == 1:
-            seqs = {
-                col: [[0] * (seq_length - len(seqs[col][0])) + seqs[col][0]]
-                for col in data_columns
-            }
-
-        return (seqs, targets)
-
-    @classmethod
-    def extract_sequences(
-        cls, data, seq_length, data_columns, target_column, return_targets
-    ):
-        raw_sequences = (
-            data.groupby("sequenceId")
-            .agg({col: list for col in data_columns})
-            .reset_index(drop=False)
+    if return_targets:
+        nseq = max(
+            len(in_seq[target_column]) - seq_length - 1,
+            min(1, len(in_seq[target_column])),
         )
 
-        rows = []
-        for _, in_row in raw_sequences.iterrows():
-            seqs, targets = cls.extract_subsequences(
-                in_row[data_columns],
-                seq_length,
-                data_columns,
-                target_column,
-                return_targets,
-            )
-            for i, target in enumerate(targets):
-                subsequence_id = i
-                for data_col, data_col_seqs in seqs.items():
-                    rows.append(
-                        [in_row["sequenceId"]]
-                        + [subsequence_id, data_col]
-                        + data_col_seqs[i]
-                        + [target if data_col == target_column else None]
-                    )
-
-        sequences = pd.DataFrame(
-            rows,
-            columns=["sequenceId", "subsequenceId", "inputCol"]
-            + list(range(seq_length, 0, -1))
-            + ["target"],
+        targets = [in_seq[target_column][i + seq_length] for i in range(nseq)]
+    else:
+        nseq = max(
+            len(in_seq[target_column]) - seq_length,
+            min(1, len(in_seq[target_column])),
         )
-        return sequences
+        targets = [np.nan for _ in range(nseq)]
 
-    @classmethod
-    def get_subset_groups(cls, sequence_data, groups, n_cols):
-        n_cases = int(sequence_data.shape[0] / n_cols)
-        subset_groups = [
-            ([i] * math.floor(n_cases * size)) for i, size in enumerate(groups)
-        ]
-        subset_groups = [inner for outer in subset_groups for inner in outer]
-        diff = n_cases - len(subset_groups)
-        subset_groups = ([0] * diff) + subset_groups
-        return subset_groups
+    seqs = {}
+    for data_col in data_columns:
+        seqs[data_col] = [in_seq[data_col][i : i + seq_length] for i in range(nseq)]
 
-    @classmethod
-    def extract_data_subsets(cls, sequences, groups):
-        assert abs(1.0 - np.sum(groups)) < 0.0000000000001, np.sum(groups)
+    if len(seqs[target_column]) == 1:
+        seqs = {
+            col: [[0] * (seq_length - len(seqs[col][0])) + seqs[col][0]]
+            for col in data_columns
+        }
 
-        datasets = [[] for _ in range(len(groups))]
-        n_cols = len(np.unique(sequences["inputCol"]))
-        for _, sequence_data in sequences.groupby("sequenceId"):
-            subset_groups = cls.get_subset_groups(sequence_data, groups, n_cols)
-            assert len(subset_groups) * n_cols == sequence_data.shape[0]
-            for i, group in enumerate(subset_groups):
-                case_start = i * n_cols
-                datasets[group].append(
-                    sequence_data.iloc[case_start : case_start + n_cols, :]
+    return (seqs, targets)
+
+
+def extract_sequences(data, seq_length, data_columns, target_column, return_targets):
+    raw_sequences = (
+        data.groupby("sequenceId")
+        .agg({col: list for col in data_columns})
+        .reset_index(drop=False)
+    )
+
+    rows = []
+    for _, in_row in raw_sequences.iterrows():
+        seqs, targets = extract_subsequences(
+            in_row[data_columns],
+            seq_length,
+            data_columns,
+            target_column,
+            return_targets,
+        )
+        for i, target in enumerate(targets):
+            subsequence_id = i
+            for data_col, data_col_seqs in seqs.items():
+                rows.append(
+                    [in_row["sequenceId"]]
+                    + [subsequence_id, data_col]
+                    + data_col_seqs[i]
+                    + [target if data_col == target_column else None]
                 )
 
-        return [pd.concat(dataset, axis=0) for dataset in datasets]
-
-
-def preprocess_batch(config, process_id, batch):
-    Preprocessor(
-        **{
-            "data": batch,
-            "process_id": process_id,
-            **config.dict(),
-        }
+    sequences = pd.DataFrame(
+        rows,
+        columns=["sequenceId", "subsequenceId", "inputCol"]
+        + list(range(seq_length, 0, -1))
+        + ["target"],
     )
+    return sequences
+
+
+def get_subset_groups(sequence_data, groups, n_cols):
+    n_cases = int(sequence_data.shape[0] / n_cols)
+    subset_groups = [
+        ([i] * math.floor(n_cases * size)) for i, size in enumerate(groups)
+    ]
+    subset_groups = [inner for outer in subset_groups for inner in outer]
+    diff = n_cases - len(subset_groups)
+    subset_groups = ([0] * diff) + subset_groups
+    return subset_groups
+
+
+def extract_data_subsets(sequences, groups):
+    assert abs(1.0 - np.sum(groups)) < 0.0000000000001, np.sum(groups)
+
+    datasets = [[] for _ in range(len(groups))]
+    n_cols = len(np.unique(sequences["inputCol"]))
+    for _, sequence_data in sequences.groupby("sequenceId"):
+        subset_groups = get_subset_groups(sequence_data, groups, n_cols)
+        assert len(subset_groups) * n_cols == sequence_data.shape[0]
+        for i, group in enumerate(subset_groups):
+            case_start = i * n_cols
+            datasets[group].append(
+                sequence_data.iloc[case_start : case_start + n_cols, :]
+            )
+
+    return [pd.concat(dataset, axis=0) for dataset in datasets]
+
+
+def preprocess_batch(
+    process_id,
+    batch,
+    split_paths,
+    seq_length,
+    data_columns,
+    target_column,
+    return_targets,
+    group_proportions,
+):
+    sequence_ids = sorted(list(np.unique(batch["sequenceId"])))
+    for i, sequence_id in enumerate(sequence_ids):
+        data_subset = batch.loc[batch["sequenceId"] == sequence_id, :]
+        sequences = extract_sequences(
+            data_subset, seq_length, data_columns, target_column, return_targets
+        )
+
+        splits = extract_data_subsets(sequences, group_proportions)
+        splits = [cast_columns_to_string(data) for data in splits]
+
+        for split_path, split in zip(split_paths, splits):
+            split_path_batch = split_path.replace(".csv", f"-{process_id}.csv")
+            write_mode = "w" if i == 0 else "a"
+            header = i == 0
+            split.to_csv(
+                split_path_batch,
+                mode=write_mode,
+                header=header,
+                sep=",",
+                decimal=".",
+                index=None,
+            )
 
 
 def get_batch_limits(data, n_batches):
@@ -268,24 +294,5 @@ def combine_multiprocessing_outputs(n_splits, n_batches):
 
 def preprocess(args, args_config):
     config = load_preprocessor_config(args.config_path, args_config)
-    data = pd.read_csv(config.data_path, sep=",", decimal=".", index_col=None)
-
-    n_cores = multiprocessing.cpu_count()
-    if config.max_rows is not None:
-        data = data.head(int(config.max_rows))
-
-    batch_limits = get_batch_limits(data, n_cores)
-    batches = [
-        (config, data.iloc[start:end, :])
-        for start, end in batch_limits
-        if (end - start) > 0
-    ]
-    batches = [(config, i, batch) for i, (config, batch) in enumerate(batches)]
-
-    n_cores_used = len(batches)
-    with multiprocessing.Pool(processes=n_cores_used) as pool:
-        pool.starmap(preprocess_batch, batches)
-
-    combine_multiprocessing_outputs(len(config.group_proportions), n_cores_used)
-
+    Preprocessor(**config.dict())
     print("Preprocessing complete")
